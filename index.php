c2d7acf88c071427f140ddea2f86705d0b3e2ef7
Scott Block <scottrblock@gmail.com>
2014-04-05 00:17:09 -0400

    pullin ze tweettzzz

--- index.php
+++ index.php
@@ -6,37 +6,47 @@
       <h1>Hello World</h1>
 
       <h2><?php echo "Hello from php";?></h2>
+      
+      <ul>
+      
+        <?php
+        #kevin was here
+          define('TWEET_LIMIT', 5);
+          define('TWITTER_USERNAME', 'theMikeSikora');
+          define('CONSUMER_KEY', 'b6J0iMKWMyeDcXKVZuxw');
+          define('CONSUMER_SECRET', 'shkIvWQnTWS2tphJQi2EASnlzGaSZecI6fvrxmHSk');
+          define('ACCESS_TOKEN', '480963993-dVWPw1Gn9gYYx0EKotTh59gdq1NmdLIoetKJUT9R');
+          define('ACCESS_TOKEN_SECRET', 'P9TciGLDqFrC83Sx5LTlx0yVlI8otiM4z6kYuSshC6o3U');
 
-      <?php
-      #kevin was here
-        define('TWEET_LIMIT', 5);
-        define('TWITTER_USERNAME', 'theMikeSikora');
-        define('CONSUMER_KEY', 'b6J0iMKWMyeDcXKVZuxw');
-        define('CONSUMER_SECRET', 'shkIvWQnTWS2tphJQi2EASnlzGaSZecI6fvrxmHSk');
-        define('ACCESS_TOKEN', '480963993-dVWPw1Gn9gYYx0EKotTh59gdq1NmdLIoetKJUT9R');
-        define('ACCESS_TOKEN_SECRET', 'P9TciGLDqFrC83Sx5LTlx0yVlI8otiM4z6kYuSshC6o3U');
+          require_once('TwitterAPIExchange.php');
 
-        require_once('TwitterAPIExchange.php');
+          /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+          $settings = array(
+              'oauth_access_token' => ACCESS_TOKEN,
+              'oauth_access_token_secret' => ACCESS_TOKEN_SECRET,
+              'consumer_key' => CONSUMER_KEY,
+              'consumer_secret' => CONSUMER_SECRET
+          );
 
-        /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
-        $settings = array(
-            'oauth_access_token' => ACCESS_TOKEN,
-            'oauth_access_token_secret' => ACCESS_TOKEN_SECRET,
-            'consumer_key' => CONSUMER_KEY,
-            'consumer_secret' => CONSUMER_SECRET
-        );
+          //https://github.com/J7mbo/twitter-api-php
 
-        //https://github.com/J7mbo/twitter-api-php
+          $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+          $getfield = '?screen_name=' . "ESPN";
+          $requestMethod = 'GET';
+          $twitter = new TwitterAPIExchange($settings);
+          $tweets = $twitter->setGetfield($getfield) 
+                       ->buildOauth($url, $requestMethod) 
+                       ->performRequest(); 
 
-        $url = 'https://api.twitter.com/1.1/followers/list.json';
-        $getfield = '?username=' . TWITTER_USERNAME;
-        $requestMethod = 'GET';
-        $twitter = new TwitterAPIExchange($settings);
-        echo $twitter->setGetfield($getfield) 
-                     ->buildOauth($url, $requestMethod) 
-                     ->performRequest(); 
+          //echo $tweets;
+          $tweets = json_decode($tweets);
+          
+          foreach($tweets as $t){
+            echo "<li>" . $t->text . "</li>";
+          }
 
-      ?>
+        ?>
+      </ul>
     </div>
   </div>
 
