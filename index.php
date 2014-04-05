6cc1b99fa07862e4beb9cfcefe6b9155980efba9
Kevin McNerney <kmm17@terpmail.umd.edu>
2014-04-05 12:47:23 -0400

    back to what we had plus removing link from tweets

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 ?>    
   <div class="container">
     <div class="col-md-12">
-      <h1>Hello World</h1>
+      <h1>Top Plays</h1>
 
-      <h2><?php echo "Hello from php";?></h2>
       
       <ul>
       
@@ -28,26 +27,82 @@
               'consumer_secret' => CONSUMER_SECRET
           );
 
-          //https://github.com/J7mbo/twitter-api-php
+         # https://github.com/J7mbo/twitter-api-php
 
-         $url = 'https://api.twitter.com/1.1/search/tweets.json';
-
-          $getfield = '?q=#sctop10&count=100&include_entities=true&mode=videos';
+          $url = 'https://api.twitter.com/1.1/search/tweets.json';
+          $getfield = '?q=%23sctop10&result_type=mixed&include_entities=true&count=200';
           $requestMethod = 'GET';
           $twitter = new TwitterAPIExchange($settings);
           $tweets = $twitter->setGetfield($getfield) 
                        ->buildOauth($url, $requestMethod) 
                        ->performRequest(); 
 
-
-          //echo $tweets;
           $tweets = json_decode($tweets);
-          
+
+          # avoid duplicates by keeping track of used youtube videos
+          $youtube_ids = array();
+
+          # loop through all tweets
           foreach($tweets->statuses as $t){
             
-            // if(isset($t->entities->media) && strlen($t->entities->media[0]->media_url) > 0){
-              echo "<pre>" . $t->text . "</pre>";
-            // }
+            # only tweets that have a link and are not retweets
+            if(strpos($t->text, 'http://t.co') !== false && substr($t->text, 0, 2) !== 'RT'){
+              # extract link from tweet
+              preg_match("/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/", $t->text, $link);
+              if(is_array($link)){
+                $flacid_url = $link[0];
+                $boner_url = expandShortUrl($link[0]);
+              }else{
+                $flacid_url = $link;
+                $boner_url = expandShortUrl($link);
+              }
+              if($boner_url === "null"){
+                continue;
+              }
+
+              # extract youtube id from link
+              $youtube_string = "http://youtu.be/";
+              if( strpos($boner_url, $youtube_string) !== false ){
+                $youtube_id = str_replace($youtube_string, "", $boner_url);
+
+                if(in_array($youtube_id, $youtube_ids)){
+                  continue;
+                }
+
+                array_push($youtube_ids, $youtube_id);
+              } else{
+                continue;
+              }
+
+              # add avi and twitter handle
+              $user_name = $t->user->screen_name;
+
+              echo "<div class='user-info'>";
+                echo "<a href='http://twitter.com/" . $user_name . "'>";
+                  echo "<img class='user-avatar' src='" . $t->user->profile_image_url . "'/>";
+                  echo "@" . $user_name;
+                echo "</a>";
+              echo "</div>";
+
+              # delete the link from the tweet
+              $linkless = str_replace($flacid_url . " ", "", $t->text);
+              echo "<div>" . $linkless . "</div>";
+              echo '<iframe width="560" height="315" src="//www.youtube.com/embed/' . $youtube_id . '" frameborder="0" allowfullscreen></iframe>';
+            }
+          }
+
+          function expandShortUrl($url) {
+            $headers = get_headers($url, 1);
+            if(isset($headers['location'])){
+              if(!is_array($headers['location'])){
+                $out = $headers['location'];
+              }else {
+                $out = $headers['location'][0];
+              }
+              return $out;
+            }else {
+              return "null";
+            }
           }
 
         ?>
