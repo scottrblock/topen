8af6e0dc8df0e8086d56127ae43b815785d6ad15
Michael Sikora <msikora.umd@gmail.com>
2014-04-04 22:08:41 -0400

    Added tokens file

---
+++ tokens.php
@@ -0,0 +1,9 @@
+<?php
+define('TWEET_LIMIT', 5);
+define('TWITTER_USERNAME', 'theMikeSikora');
+define('CONSUMER_KEY', 'n5SbbMJCKFEHXlPHt5ELSjCVN');
+define('CONSUMER_SECRET', 'd2bPuSfHRM8UtzJDwiEBXClLWRQ7VHdaTbqI9BCl7tZ5eIJmK4');
+define('ACCESS_TOKEN', '480963993-3jvB2ikkBkqCizENWiBVraotcIdHziuKjw2ftOq4');
+define('ACCESS_TOKEN_SECRET', 'B3omhGUKDLu0dJgwcRVsN290FaeYTCz7sKkWleQBYOm4Y');
+
+?>
